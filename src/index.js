d5a3e1609512caffcd53039a7561744744626dab
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-04-07 22:31:33 -0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 
 // Initilizations
@@ -10,7 +12,16 @@ require('./config/database');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-// app.set('public', path.join(__dirname, 'public'));
+
+// Configuration of the Handlebars template engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.html', exphbs({
+    defaultLayout: false,
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.html'
+}));
+app.set('view engine', '.html');
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
@@ -21,8 +32,14 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
 
 // Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+})
 
 // Routes
 app.use(require('./routes/home_admin'));
