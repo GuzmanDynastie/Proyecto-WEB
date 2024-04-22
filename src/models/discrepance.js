b2ab8d96abca1c833025443530ede6341c5fda69
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-04-21 23:32:03 -0600

    Have been added the atribute Date

--- src/models/discrepance.js
+++ src/models/discrepance.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const discrepanceSchema = new Schema({
-    discrepance: { type: String, required: true}
+    discrepance: { type: String, required: true},
+    date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('discrepance', discrepanceSchema);
+module.exports = mongoose.model('discrepances', discrepanceSchema);
