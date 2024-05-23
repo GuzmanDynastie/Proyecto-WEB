aba4e5c664f72ef877f20fe83d5ad451ecd045fb
GuzmanDynastie LINUX <guzmanjrpro@gmail.com>
2024-05-22 21:44:46 -0600

    modifications

--- src/routes/chatBot.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const product = require('../models/product')
-
-router.get('/chatbot/productos', async (req, res) => {
-  const products= await product.find();
-  res.json(products);
-});
-
-module.exports = router;
