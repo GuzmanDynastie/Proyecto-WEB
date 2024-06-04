dc06caa9cbe95e11c792424e6f9bbf5af3ff7e67
GuzmanDynastie <guzmanjrpro@gmail.com>
2024-06-04 16:31:53 -0600

    new name to webhook

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -26,19 +26,16 @@ router.post('/webhook', async (req, res) => {
 async function handleDetailsProduct(req, res) {
     try {
         const { marca: nameBrandSolicited, raza: breedSolicited, etapa: petStage } = req.body;
-    
-        // Convertir a minúsculas para la búsqueda
-        const searchTerms = [nameBrandSolicited.trim().toLowerCase(), breedSolicited.trim().toLowerCase(), petStage.trim().toLowerCase()];
-    
-        // Buscar todos los productos en la BD
+
         const productsInBD = await productSchema.find();
-    
-        // Comprobar si existe la marca
-        const brandExists = productsInBD.some(product => product.generalCharacteristics[1].toLowerCase() === nameBrandSolicited.trim().toLowerCase());
-    
+        const brandExists = productsInBD.some(product => {
+            return product.generalCharacteristics.includes(nameBrandSolicited);
+        });
+
         let query = { status: true };
-    
+
         if (nameBrandSolicited && breedSolicited && petStage) {
+            const searchTerms = [nameBrandSolicited.trim().toLowerCase(), breedSolicited.trim().toLowerCase(), petStage.trim().toLowerCase()];
             const orConditions = searchTerms.map(term => ({
                 $or: [
                     { 'petCharacteristics.0': { $regex: new RegExp(term, 'i') } },
@@ -48,10 +45,10 @@ async function handleDetailsProduct(req, res) {
             }));
             query.$and = orConditions;
         }
-    
+
         if (brandExists) {
             const products = await productSchema.find(query).lean();
-    
+
             if (products.length > 0) {
                 const formattedProducts = products.map(product => ({
                     Marca: product.generalCharacteristics[1],
@@ -59,22 +56,20 @@ async function handleDetailsProduct(req, res) {
                     Categoria: product.petCharacteristics[1],
                     Sabor: product.specifications[0],
                     Peso: product.specifications[1],
-                    Imagen: product.images[0]
+                    Imagen: product.images[0],
+                    ID_product: product._id
                 }));
-    
+
                 res.json({
                     mensaje: `Los productos que coinciden son:<br><hr> 
-                    ${formattedProducts.map(p => `
-                    - <strong>Marca:</strong> ${p.Marca}<br>
-                    - <strong>Raza:</strong> ${p.Raza}<br>
-                    - <strong>Categoria:</strong> ${p.Categoria}<br>
-                    - <strong>Sabor:</strong> ${p.Sabor}<br>
-                    - <strong>Peso:</strong> ${p.Peso}<br>
-                    `).join('<hr>')}`,
+                    - <strong>Marca:</strong> ${formattedProducts[0].Marca}
+                    - <strong>Raza:</strong> ${formattedProducts[0].Raza}
+                    - <strong>Categoria:</strong> ${formattedProducts[0].Categoria}
+                    - <strong>Sabor:</strong> ${formattedProducts[0].Sabor}
+                    - <strong>Peso:</strong> ${formattedProducts[0].Peso}`,
                     image: `https://nutripet-healthy.up.railway.app/${formattedProducts[0].Imagen}`,
+                    url: `https://nutripet-healthy.up.railway.app/shopping/shop/${formattedProducts[0].ID_product}`
                 });
-            } else {
-                res.json({ mensaje: 'No existen productos que coincidan con los criterios de búsqueda.' });
             }
         } else {
             res.json({ mensaje: 'No existen productos que coincidan con los criterios de búsqueda.' });
@@ -82,7 +77,7 @@ async function handleDetailsProduct(req, res) {
     } catch (error) {
         console.error('Error al procesar la acción:', error);
         res.status(500).json({ error: 'Error al procesar la acción.' });
-    }    
+    }
 }
 
 module.exports = router;
