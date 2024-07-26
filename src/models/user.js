c5b268c373f657c4547317b4aeeb7686ac294681
GuzmanDynastie <guzmanjrpro@gmail.com>
2024-07-26 00:53:47 -0600

    Have been added the order model

--- src/models/user.js
+++ src/models/user.js
@@ -3,19 +3,19 @@ const { Schema } = mongoose;
 const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
-    name: { type: String, required: true, maxlength: 50},
+    name: { type: String, required: true, maxlength: 50 },
     surname: { type: String, required: true, maxlength: 50 },
     email: { type: String, required: true, maxlength: 50 },
     password: { type: String, required: true },
-    image: { type: String, required: false , default: ''},
+    image: { type: String, required: false, default: '' },
     role: { type: String, required: true, default: 'user' },
     status: { type: Boolean, required: true, default: true }
 });
 
 userSchema.methods.encryptPassword = async (password) => {
-   const salt = await bcrypt.genSalt(10);
-   const hash = await bcrypt.hash(password, salt);
-   return hash;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
 };
 
 userSchema.methods.matchPassword = async function (password) {
