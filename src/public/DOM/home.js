91952304d7b8d281239717696e123db36343d915
GuzmanDynastie <guzmanjrpro@gmail.com>
2024-08-04 13:19:21 -0600

    feat: add 'scroll' function to handle smooth scrolling in home.js

---
+++ src/public/DOM/home.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function () {
+    var secondNavbar = document.getElementById('second-navbar');
+    var contentAfterNavar = secondNavbar.nextElementSibling;
+    var offsetTop = secondNavbar.getAttribute('data-offset-top') || secondNavbar.offsetTop;
+
+    if (!secondNavbar.getAttribute('data-offset-top')) {
+        secondNavbar.setAttribute('data-offset-top', offsetTop);
+    }
+
+    if (window.pageYOffset >= offsetTop) {
+        secondNavbar.classList.add('fixed');
+        contentAfterNavar.style.marginTop = secondNavbar.offsetHeight + 'px';
+    } else {
+        secondNavbar.classList.remove('fixed');
+        contentAfterNavar.style.marginTop = '0';
+    }
+});
