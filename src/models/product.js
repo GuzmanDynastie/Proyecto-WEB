9df166e5fb71ac0774d34bbe34643ac197d19ae0
GuzmanDynastie <guzmanjrpro@gmail.com>
2024-08-03 23:18:01 -0600

    style: format code to schema

--- src/models/product.js
+++ src/models/product.js
@@ -8,10 +8,26 @@ const productSchema = new Schema({
     pricePerUnit: { type: Number, required: true },
     expiration: { type: Date, required: true },
     principalCharacteristics: { type: String, required: false, default: ''},
-    petCharacteristics: [{ type: String, required: false, default: '' }],
-    specifications: [{ type: String, required: false, default: '' }],
-    generalCharacteristics: [{ type: String, required: false, default: '' }],
-    others: [{ type: String, required: false, default: '' }],
+    petCharacteristics: [{ 
+        type: String, 
+        required: false, 
+        default: '' 
+    }],
+    specifications: [{ 
+        type: String, 
+        required: false, 
+        default: '' 
+    }],
+    generalCharacteristics: [{ 
+        type: String, 
+        required: false, 
+        default: '' 
+    }],
+    others: [{ 
+        type: String, 
+        required: false, 
+        default: '' 
+    }],
     description: { type: String, required: true },
     status: { type: Boolean, required: true, default: true }
 });
