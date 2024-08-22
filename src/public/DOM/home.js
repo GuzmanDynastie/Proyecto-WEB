3dfff2724bd3cbcf66fcd14d81ad382cce33645b
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-08-21 22:54:22 -0600

    test: add test

--- src/public/DOM/home.js
+++ src/public/DOM/home.js
@@ -1,12 +1,32 @@
-function handleScroll() {
+// function handleScroll() {
+//     var secondNavbar = document.getElementById('second-navbar');
+//     var contentAfterNavar = secondNavbar.nextElementSibling;
+//     var offsetTop = secondNavbar.getAttribute('data-offset-top') || secondNavbar.offsetTop;
+
+//     if (!secondNavbar.getAttribute('data-offset-top')) {
+//         secondNavbar.setAttribute('data-offset-top', offsetTop);
+//     }
+
+//     if (window.pageYOffset >= offsetTop) {
+//         secondNavbar.classList.add('fixed');
+//         contentAfterNavar.style.marginTop = secondNavbar.offsetHeight + 'px';
+//     } else {
+//         secondNavbar.classList.remove('fixed');
+//         contentAfterNavar.style.marginTop = '0';
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     window.addEventListener('scroll', handleScroll);
+// });
+
+window.addEventListener('scroll', function () {
     var secondNavbar = document.getElementById('second-navbar');
     var contentAfterNavar = secondNavbar.nextElementSibling;
     var offsetTop = secondNavbar.getAttribute('data-offset-top') || secondNavbar.offsetTop;
-
     if (!secondNavbar.getAttribute('data-offset-top')) {
         secondNavbar.setAttribute('data-offset-top', offsetTop);
     }
-
     if (window.pageYOffset >= offsetTop) {
         secondNavbar.classList.add('fixed');
         contentAfterNavar.style.marginTop = secondNavbar.offsetHeight + 'px';
@@ -14,8 +34,4 @@ function handleScroll() {
         secondNavbar.classList.remove('fixed');
         contentAfterNavar.style.marginTop = '0';
     }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', handleScroll);
 });
--- src/public/DOM/shop.js
+++ src/public/DOM/shop.js
@@ -16,6 +16,24 @@ function handleScroll() {
     }
 }
 
+
+window.addEventListener('scroll', function () {
+    var secondNavbar = document.getElementById('second-navbar');
+    var contentAfterNavar = secondNavbar.nextElementSibling;
+    var offsetTop = secondNavbar.getAttribute('data-offset-top') || secondNavbar.offsetTop;
+    if (!secondNavbar.getAttribute('data-offset-top')) {
+        secondNavbar.setAttribute('data-offset-top', offsetTop);
+    }
+    if (window.pageYOffset >= offsetTop) {
+        secondNavbar.classList.add('fixed');
+        contentAfterNavar.style.marginTop = secondNavbar.offsetHeight + 'px';
+    } else {
+        secondNavbar.classList.remove('fixed');
+        contentAfterNavar.style.marginTop = '0';
+    }
+});
+
+
 function submitInformationForm(search) {
     const form = document.createElement('form');
     form.action = '/shopping/shop';
@@ -31,6 +49,6 @@ function submitInformationForm(search) {
     form.submit();
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', handleScroll);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     window.addEventListener('scroll', handleScroll);
+// });
